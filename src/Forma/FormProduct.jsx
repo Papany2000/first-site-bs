1482a74061145309f853111c960c35f8d41a2004
Sergei <Papany2000@ramble.ru>
2024-02-07 22:27:59 +0300

    use initData

--- src/Forma/FormProduct.jsx
+++ src/Forma/FormProduct.jsx
@@ -1,44 +1,79 @@
-import React, { useEffect } from 'react';
-import './Form.css'
+import React, {useCallback, useEffect, useState} from 'react';
+import './Form.css';
 import { useTelegram } from '../hooks/useTelegram';
 
 
-function FormProductList() {
+const FormProductList = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
 
-    const[country, setCountry] = React.useState('')
-    const[street, setStreet] = React.useState('')
-    const[subject, setSubject] = React.useState('phisical')
-    const{tg} = useTelegram()
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
 
-    React.useEffect(() => {
-        // меняем текст кнопки
-        tg.MainButton?.setParams({
-            text: 'отправить данные'
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
         })
     }, [])
-     // сделаем прocтую валидацию
-     React.useEffect(() => {
+
+    useEffect(() => {
         if(!street || !country) {
-            tg.MainButton?.hide()
+            tg.MainButton.hide();
         } else {
-           tg.MainButton?.show()
+            tg.MainButton.show();
         }
-    }, [street, country])
-    const onChangeCountry = (e) => setCountry(e.target.value)
-    const onChangeStreet = (e) => setStreet(e.target.value)
-    const onChangeSubject = (e) => setSubject(e.target.value)
-  
+    }, [country, street])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
     return (
-      <div className="form">
-        <h3>Введите ваши данные </h3>
-        <input className={'input'} type="text" placeholder={'страна'} value={country} onChange={onChangeCountry}  />
-        <input className={'input'} type="text" placeholder={'улица'} value={street} onChange={onChangeStreet} />
-        <select className={'select'} value={subject} onChange={onChangeSubject}>
-            <option value={'phisical'}>физ. лицо</option>
-            <option value={'legal'}>юр. лицо</option>
-        </select>
-      </div>
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
     );
-  }
-  
-  export default FormProductList;
+};
+
+export default FormProductList;
