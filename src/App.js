1ad7d57585aa52c679b6ddfefacd1072b1285e63
Sergei <Papany2000@ramble.ru>
2024-02-08 16:41:37 +0300

    use initData

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Header from './Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import ProductList from './Product/ProductList';
 import FormProductList from './Forma/FormProduct';
+import { useTelegram } from './hooks/useTelegram';
+import React from 'react';
 
 
 
 function App() {
 
-
+const {onClose} = useTelegram()
+React.useEffect(() => {
+  tg.ready()
+}, [])
 
 
   return (
     <div className="App">
+      <button onClick={onClose}> Закрыть</button>
      <Header/>
      <Routes>
         <Route path='/' element = {<ProductList/>}/>
