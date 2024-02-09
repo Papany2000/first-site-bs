bd2b5520ff8c6be2d3c42d4347234fcab7cf575f
Sergei <Papany2000@ramble.ru>
2024-02-09 15:39:07 +0300

    use initData

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@
 import './App.css';
 import Header from './Header/Header';
 import { Route, Routes } from 'react-router-dom';
-import ProductList from './Product/ProductList';
 import FormProductList from './Forma/FormProduct';
 import { useTelegram } from './hooks/useTelegram';
 import React from 'react';
-import Button from './Button/Button';
+import ProductList from './ProductList/ProductList';
 
 
 function App() {
 
-  const {onToogleButton} = useTelegram()
+
 
 const {tg} = useTelegram()
 React.useEffect(() => {
@@ -22,7 +21,6 @@ React.useEffect(() => {
   return (
     <div className="App">
      <Header/>
-     <Button onClick={onToogleButton}>toogle</Button>
      <Routes>
         <Route index element = {<ProductList/>}/>
         <Route path='/form' element = {<FormProductList/>}/>
--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
+import { useTelegram } from '../hooks/useTelegram';
 
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
